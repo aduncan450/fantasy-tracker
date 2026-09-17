9501ed998abf3d7eb2823765b20ded0b8ee3dc64
aduncan450 <aduncan450@gmail.com>
2026-09-17 11:46:11 -0500

    Connect frontend to Supabase

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const SUPABASE_URL='';
-export const SUPABASE_ANON_KEY='';
+export const SUPABASE_URL='https://eyjmpuwfbqzvjqcxblza.supabase.co';
+export const SUPABASE_ANON_KEY='sb_publishable_gIx3ux0JKsefrLluT4soVQ_JL0eSZ1E';
 export const LEAGUE_ID='buff-husky-2026';
 export const isConfigured=()=>Boolean(SUPABASE_URL&&SUPABASE_ANON_KEY);
