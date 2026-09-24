c464967648ffe2ffc98a2cf37ea4479a566b5d13
aduncan450 <aduncan450@gmail.com>
2026-09-23 21:49:36 -0500

    Route admin saves through TEST MODE fail-closed wrapper

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-import {loadAdminLeague,saveAdminLeague,signIn,captureSessionFromHash,signOut,isTestMode,enterTestMode,resetTestMode,exitTestMode} from './storage.js?v=20260923-compact-parlay1';
+import {loadAdminLeague,saveAdminLeague,signIn,captureSessionFromHash,signOut,isTestMode,enterTestMode,resetTestMode,exitTestMode} from './storage-admin.js?v=20260923-testmode-isolation1';
 import {money,pot,currentWeek,weekResult,duesRows} from './calculations.js?v=20260920-live-sleeper';
 import {normalizeLeague,validateLeague} from './schema.js?v=20260923-compact-parlay1';
 import {SLEEPER_LEAGUE_ID,sleeperTeams,syncSleeperScores} from './sleeper.js?v=20260920-live-sleeper';
